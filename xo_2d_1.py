beb7c7a7c526c38e9e7ea48dab5cdecce71ef8fa
Sean Morrissey-Ralevic <smorrisseyralevic@gmail.com>
2025-08-16 16:06:26 +0100

    add bugged move validation

--- xo_2d_1.py
+++ xo_2d_1.py
@@ -2,29 +2,6 @@
 
 import numpy as np
 
-'''
-board = [["-", "-", "-"], ["-", "-", "-"], ["-", "-", "-"]]
-
-for i in range(9):
-    turn = ["X", "O"][i % 2]
-    move = int(input("Enter your move 1-9."))
-    board[(move-1)//3][(move-1)%3] = turn
-    print(board)
-    for j in range(3):
-        if board[j][0] == board[j][1] == board[j][2] == turn:
-            print(f'{turn} wins!')
-            break
-        elif board[0][j] == board[1][j] == board[2][j] == turn:
-            print(f'{turn} wins!')
-            break
-    if board[0][0] == board[1][1] == board[2][2] == turn:
-        print(f'{turn} wins!')
-        break
-    if board[2][0] == board[1][1] == board[0][2] == turn:
-        print(f'{turn} wins!')
-        break
-'''
-
 def run_xo():
 
     x = int(input("Enter board width."))
@@ -33,7 +10,13 @@ def run_xo():
     
     for i in range(9):
         turn = ["X", "O"][i % 2]
-        move = int(input("Enter your move 1-9."))
+        move_valid = False
+
+        while not move_valid:
+            move = int(input("Enter your move 1-9."))
+            if move_valid
+
+
         board[(move-1)//3][(move-1)%3] = turn
         print(board)
         for j in range(3):
