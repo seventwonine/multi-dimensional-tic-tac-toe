c6c7d133fb276846e18a69c2122126361b0852a6
Sean Morrissey-Ralevic <smorrisseyralevic@gmail.com>
2025-08-16 15:57:53 +0100

    rewrite as function

--- xo_2d_1.py
+++ xo_2d_1.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 
+'''
 board = [["-", "-", "-"], ["-", "-", "-"], ["-", "-", "-"]]
 
 for i in range(9):
@@ -22,4 +23,31 @@ for i in range(9):
     if board[2][0] == board[1][1] == board[0][2] == turn:
         print(f'{turn} wins!')
         break
+'''
 
+def run_xo():
+
+    x = int(input("Enter board width."))
+    y = int(input("Enter board height."))
+    board = [["-", "-", "-"], ["-", "-", "-"], ["-", "-", "-"]]
+    
+    for i in range(9):
+        turn = ["X", "O"][i % 2]
+        move = int(input("Enter your move 1-9."))
+        board[(move-1)//3][(move-1)%3] = turn
+        print(board)
+        for j in range(3):
+            if board[j][0] == board[j][1] == board[j][2] == turn:
+                print(f"{turn} wins!")
+                return
+            elif board[0][j] == board[1][j] == board[2][j] == turn:
+                print(f"{turn} wins!")
+                return
+        if board[0][0] == board[1][1] == board[2][2] == turn:
+            print(f"{turn} wins!")
+            return
+        if board[2][0] == board[1][1] == board[0][2] == turn:
+            print(f"{turn} wins!")
+            return
+
+run_xo()
